90a5815e1d79e362f01f9407e635527797d67cfd
Akbar S <54757270+akbartus@users.noreply.github.com>
2023-05-29 10:20:45 -0500

    Updated ver. of the component

--- js/clipping-plane-component.js
+++ js/clipping-plane-component.js
@@ -1,151 +1,154 @@
 AFRAME.registerComponent('clipping-plane', {
     schema: {
-        gltfURL: { type: 'string', default: '' },
-        gltfPosition: { type: 'vec3', default: { x: 0, y: 0, z: 0 } },
-        gltfScale: { type: 'vec3', default: { x: 1, y: 1, z: 1 } },
         clippingDirection: { type: 'string', default: 'top-to-bottom' },
-        gltfMatSide: { type: 'boolean', default: false },
+        materialSide: { type: 'boolean' },
         planeConstant: { type: 'float', default: 2.0 }, // from where it should start
-        minScrollValue: { type: 'float', default: -1.0 }, // min position on y axis
-        maxScrollValue: { type: 'float', default: 2.0 }, // // max position on y axis
+        minScrollValue: { type: 'float', default: -2.0 }, // min position on y axis
+        maxScrollValue: { type: 'float', default: 2.0 }, // max position on y axis
         mouseScrollSpeed: { type: 'float', default: 0.0005 },
-        touchScrollSpeed: { type: 'float', default: 0.01 }
+        touchScrollSpeed: { type: 'float', default: 0.01 },
     },
+
     init: function () {
-        let scene = document.querySelector("a-scene").object3D;
-        let object;
-        let localPlane;
-        let matrix;
-        //let clippingPl = this.data.clippingPlaneEnabled;
-        let gltfPos = this.data.gltfPosition;
-        let gltfScl = this.data.gltfScale;
-        let gltfSide = this.data.gltfMatSide;
+        let cam = document.querySelector("a-camera");
+        cam.setAttribute("cursor", "rayOrigin: mouse;");
+        cam.setAttribute("raycaster", "objects: .clickable");
+        let matSide = this.data.materialSide;
         let minScroll = this.data.minScrollValue;
         let maxScroll = this.data.maxScrollValue;
         let mScroll = this.data.mouseScrollSpeed;
         let tScroll = this.data.touchScrollSpeed;
+        let matrix;
 
-        let cam = document.querySelector("a-camera");
-        cam.setAttribute("cursor", "rayOrigin: mouse;");
-        cam.setAttribute("raycaster", "objects: .clickable");
+        const allEntities = document.querySelectorAll("a-entity");
+      
 
-        if (this.data.clippingDirection == "top-to-bottom") {
-            // Top to bottom
-            localPlane = new THREE.Plane(new THREE.Vector3(-1, 0, 0));
-            localPlane.constant = this.data.planeConstant;
-            matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        } else if (this.data.clippingDirection == "bottom-to-top") {
-            // Bottom to top
-            localPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0));
-            localPlane.constant = this.data.planeConstant;
-            matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        } else if (this.data.clippingDirection == "left-to-right") {
-            // Left to right
-            localPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0)); // plane with normal vector along the x-axis
-            localPlane.constant = this.data.planeConstant;
-            matrix = new THREE.Matrix4().makeRotationX(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        } else if (this.data.clippingDirection == "right-to-left") {
-            // Right to left
-            localPlane = new THREE.Plane(new THREE.Vector3(-1, 0, 0)); // plane with normal vector along the x-axis
-            localPlane.constant = this.data.planeConstant; // set the distance from the origin
-            matrix = new THREE.Matrix4().makeRotationX(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        } else if (this.data.clippingDirection == "front-to-back") {
-            // Front to back
-            localPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0));
-            localPlane.constant = this.data.planeConstant;
-            matrix = new THREE.Matrix4().makeRotationY(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        } else if (this.data.clippingDirection == "back-to-front") {
-            // Back to front
-            localPlane = new THREE.Plane(new THREE.Vector3(-1, 0, 0));
-            localPlane.constant = this.data.planeConstant;
-            matrix = new THREE.Matrix4().makeRotationY(Math.PI / 2);
-            localPlane.normal.applyMatrix4(matrix);
-        }
+        // Set up event listeners for each entity
+        for (let i = 0; i < allEntities.length; i++) {
 
-        // Load model using GLTFLoader
-        let gltfEntity = document.createElement('a-entity');
-        gltfEntity.setAttribute('gltf-model', `${this.data.gltfURL}`);
-        gltfEntity.className = "clickable";
-        gltfEntity.addEventListener('model-loaded', (e) => {
-            object = e.detail.model;
-            object.traverse(function (node) {
-                if (node.isMesh) {
-                    let meshMaterial = node.material.clone(); // clone original material
-                    meshMaterial.clippingPlanes = [localPlane]; // set clipping planes for the mesh material
-                    meshMaterial.clipShadows = true;
-                    if(gltfSide){
-                        meshMaterial.side = THREE.DoubleSide;
-                    }
-                    node.material = meshMaterial; // set the cloned material as the mesh's material
-                    node.castShadow = true;
-                    node.position.x = gltfPos.x;
-                    node.position.y = gltfPos.y;
-                    node.position.z = gltfPos.z;
-                    node.scale.x = gltfScl.x;
-                    node.scale.y = gltfScl.y;
-                    node.scale.z = gltfScl.z;
-                }
-            });
-            scene.add(object); // Add the model to the A-Frame scene
-        });
-        this.el.appendChild(gltfEntity);
-
-
-        // Renderer
-        let renderer = document.querySelector("a-scene").renderer;
-        renderer.shadowMap.enabled = true;
-        // Clipping setup (renderer)
-
-        Empty = Object.freeze([]);
-        renderer.clippingPlanes = Empty;
-        renderer.localClippingEnabled = true;
-
-        // Event listeners for mousewheel and touchmove
-        gltfEntity.addEventListener("click", function () {
-            document.addEventListener("mousewheel", mouseScroll);
-            document.addEventListener("touchmove", touchMove);
-        });
-        function mouseScroll(event) {
-            const MIN_SCROLL = minScroll; 
-            const MAX_SCROLL = maxScroll; 
-            const delta = event.wheelDeltaY || event.deltaY || 0;
-            const deltaConstant = delta * mScroll; 
-            localPlane.constant += deltaConstant;
-            if (localPlane.constant < MIN_SCROLL) {
-                localPlane.constant = MIN_SCROLL;
-            } else if (localPlane.constant > MAX_SCROLL) {
-                localPlane.constant = MAX_SCROLL;
+            const localPlanes = [];
+            const clippingDir = allEntities[i].getAttribute("clipping-plane").clippingDirection;
+            // Create a new THREE.Plane object for each gltf model and push it to the localPlanes array
+            let localPlane;
+            if (clippingDir == "top-to-bottom") {
+                // Top to bottom
+                localPlane = new THREE.Plane(new THREE.Vector3(-1, 0, 0));
+                localPlane.constant = this.data.planeConstant;
+                matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
+            } else if (clippingDir == "bottom-to-top") {
+                // Bottom to top
+                localPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0));
+                localPlane.constant = this.data.planeConstant;
+                matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
+            } else if (clippingDir == "left-to-right") {
+                // Left to right
+                localPlane = new THREE.Plane(new THREE.Vector3(0, 2, 0)); // plane with normal vector along the x-axis
+                localPlane.constant = this.data.planeConstant;
+                matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
+            } else if (clippingDir == "right-to-left") {
+                // Right to left
+                localPlane = new THREE.Plane(new THREE.Vector3(0, -2, 0)); // plane with normal vector along the x-axis
+                localPlane.constant = this.data.planeConstant; // set the distance from the origin
+                matrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
+            } else if (clippingDir == "front-to-back") {
+                // Front to back
+                localPlane = new THREE.Plane(new THREE.Vector3(1, 0, 0));
+                localPlane.constant = this.data.planeConstant;
+                matrix = new THREE.Matrix4().makeRotationY(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
+            } else if (clippingDir == "back-to-front") {
+                // Back to front
+                localPlane = new THREE.Plane(new THREE.Vector3(-1, 0, 0));
+                localPlane.constant = this.data.planeConstant;
+                matrix = new THREE.Matrix4().makeRotationY(Math.PI / 2);
+                localPlane.normal.applyMatrix4(matrix);
             }
-        }
-        let prevTouchY;
-        function touchMove(event) {
-            const MIN_SCROLL = minScroll; 
-            const MAX_SCROLL = maxScroll; 
-            // Get current touch position
-            if (!event.targetTouches.length) return;
-            const currTouchY = event.targetTouches[0].clientY;
-            let deltaY;
-            if (prevTouchY === undefined) {
-                deltaY = 1; // initialize to 1 on first call
+
+            localPlanes.push(localPlane);
+            // Set Clipping Plane for GLTF 
+            if (allEntities[i].getAttribute("gltf-model")) {
+                allEntities[i].addEventListener('model-loaded', (e) => {
+                    const object = e.detail.model;
+                    object.traverse(function (node) {
+                        if (node.isMesh && localPlane) {
+                            let meshMaterial = node.material.clone();
+                            meshMaterial.clippingPlanes = localPlanes;
+                            meshMaterial.clipShadows = true;
+                            if (matSide) {
+                                meshMaterial.side = THREE.DoubleSide;
+                            }
+                            node.material = meshMaterial;
+                            node.castShadow = true;
+                        }
+                    })
+                });
             } else {
-                deltaY = prevTouchY - currTouchY;
+                // Set clipping plane for custom entity (not GLTF)
+                if (allEntities[i] != null) {
+
+                    if (allEntities[i].object3D.children) {
+                        allEntities[i].addEventListener('loaded', (e) => {
+                            allEntities[i].object3D.children[0].material.clippingPlanes = localPlanes;
+                            if (matSide == true) {
+                                allEntities[i].object3D.children[0].material.side = THREE.DoubleSide;
+                            }
+                            allEntities[i].object3D.children[0].material.clipShadows = true;
+
+                        })
+                    }
+                }
             }
-            prevTouchY = currTouchY;
-            const deltaConstant = deltaY * tScroll; 
-            localPlane.constant += deltaConstant;
-            if (localPlane.constant < MIN_SCROLL) {
-                localPlane.constant = MIN_SCROLL;
-            } else if (localPlane.constant > MAX_SCROLL) {
-                localPlane.constant = MAX_SCROLL;
+            // Set up event listeners for mousewheel and touchmove on click
+            allEntities[i].addEventListener("click", function () {
+                document.addEventListener("mousewheel", mouseScroll);
+                document.addEventListener("touchmove", touchMove);
+            });
+
+            function mouseScroll(event) {
+                const delta = event.wheelDeltaY || event.deltaY || 0;
+                const deltaConstant = delta * mScroll;
+
+                if (localPlane) {
+                    localPlane.constant += deltaConstant;
+                    if (localPlane.constant < minScroll) {
+                        localPlane.constant = minScroll;
+                    } else if (localPlane.constant > maxScroll) {
+                        localPlane.constant = maxScroll;
+                    }
+                }
             }
 
+            let prevTouchY;
+            function touchMove(event) {
+                // Get current touch position
+                if (!event.targetTouches.length) return;
+                const currTouchY = event.targetTouches[0].clientY;
+                let deltaY;
+                if (prevTouchY === undefined) {
+                    deltaY = 1; // initialize to 1 on first call
+                } else {
+                    deltaY = prevTouchY - currTouchY;
+                }
+                prevTouchY = currTouchY;
+                const deltaConstant = deltaY * tScroll;
+                if (localPlane) {
+                    localPlane.constant += deltaConstant;
+                    if (localPlane.constant < minScroll) {
+                        localPlane.constant = minScroll;
+                    } else if (localPlane.constant > maxScroll) {
+                        localPlane.constant = maxScroll;
+                    }
+                }
+            }
         }
 
+        // Enable local clipping and set the clipping planes to an empty array
+        const renderer = document.querySelector("a-scene").renderer;
+        renderer.localClippingEnabled = true;
+        renderer.clippingPlanes = [];
     },
-
 });
